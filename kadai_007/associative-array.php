806fbcfc13e107f03de4d615ba22b622eb410199
sorayoumiyo <miwamiwabiwako@gmail.com>
2024-10-19 22:05:43 +0900

    ファイルを追加

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>kadai_007</title>
+</head>
+<body>
+    <p>
+        <?php
+        $vegetable_date = ['name'=>'onion','price'=>'200','weight'=>'160'];
+        print_r($vegetable_date);
+        ?>
+    </p>
+    
+</body>
+</html>
